c483433448696aa7acfde4c48c857c3f7ae30756
Dan <themighty1@users.noreply.github.com>
2022-02-23 10:49:15 +0000

    Update backends.rs

--- chacha20/src/backends.rs
+++ chacha20/src/backends.rs
@@ -1,21 +1,9 @@
 use cfg_if::cfg_if;
 
 cfg_if! {
-    if #[cfg(chacha20_force_soft)] {
+    if #[cfg(chacha20_force_soft______)] {
         pub(crate) mod soft;
-    } else if #[cfg(any(target_arch = "x86", target_arch = "x86_64"))] {
-        cfg_if! {
-            if #[cfg(chacha20_force_avx2)] {
-                pub(crate) mod avx2;
-            } else if #[cfg(chacha20_force_sse2)] {
-                pub(crate) mod sse2;
-            } else {
-                pub(crate) mod soft;
-                pub(crate) mod avx2;
-                pub(crate) mod sse2;
-            }
-        }
     } else {
-        pub(crate) mod soft;
+        pub(crate) mod sse2;
     }
 }
